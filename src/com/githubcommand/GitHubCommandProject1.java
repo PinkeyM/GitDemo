6b144e77c5a5ba69a6ed669eca433630f8d6f97c
Pinkey Mahto <mahtopinkey@gmail.com>
2020-10-16 16:58:10 +0530

    3rd commit

--- src/com/githubcommand/GitHubCommandProject1.java
+++ src/com/githubcommand/GitHubCommandProject1.java
@@ -5,6 +5,7 @@ public class GitHubCommandProject1 {
 	public static void main(String[] args) {
 		System.out.println("Git Command");
 		System.out.println("New file");
+		System.out.println("3rd commit");
 
 	}
 
